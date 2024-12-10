cb698c4a7b5059afdd4979efba5e3e76b446c2be
chris burzo <cburzo1@wgu.edu>
2024-12-10 14:24:32 -0500

    6

--- main.py
+++ main.py
@@ -52,6 +52,32 @@ class Package:
     def __str__(self):
         return "%s, %s, %s, %s, %s, %s, %s" % (self.ID, self.addr, self.city, self.zipcode,self.deadline, self.weight, self.status)
 
+class Vertex:
+    # Constructor for a new Vertx object. All vertex objects
+    # start with a distance of positive infinity.
+    def __init__(self, label):
+        self.label = label
+        self.distance = float('inf')
+        self.pred_vertex = None
+
+class Graph:
+    def __init__(self):
+        self.adjacency_list = {}  # vertex dictionary {key:value}
+        self.edge_weights = {}  # edge dictionary {key:value}
+
+    def add_vertex(self, new_vertex):
+        self.adjacency_list[new_vertex] = []  # {vertex_1: [], vertex_2: [], ...}
+
+    def add_directed_edge(self, from_vertex, to_vertex, weight=1.0):
+        self.edge_weights[(from_vertex, to_vertex)] = weight
+        # {(vertex_1,vertex_2): 484, (vertex_1,vertex_3): 626, (vertex_2,vertex_6): 1306, ...}
+        self.adjacency_list[from_vertex].append(to_vertex)
+        # {vertex_1: [vertex_2, vertex_3], vertex_2: [vertex_6], ...}
+
+    def add_undirected_edge(self, vertex_a, vertex_b, weight=1.0):
+        self.add_directed_edge(vertex_a, vertex_b, weight)
+        self.add_directed_edge(vertex_b, vertex_a, weight)
+
 def loadPackageCSV(filename):
     with open(filename) as PackageCSV:
         packageData = csv.reader(PackageCSV, delimiter=',')
@@ -76,11 +102,25 @@ def loadPackageCSV(filename):
 def packageLookUp(ID):
     return myHash.search(ID)
 
+
+
 myHash = ChainingHashTable()
 
 loadPackageCSV("PackageCSV.csv")
 
-print(packageLookUp(3))
+g = Graph()
+
+vertex_1 = Vertex("1")
+g.add_vertex(vertex_1)
+
+vertex_2 = Vertex("2")
+g.add_vertex(vertex_2)
+
+g.add_undirected_edge(vertex_1, vertex_2, 484)
+
+print(vertex_2.label)
+
+#print(packageLookUp(3))
 
 #for i in range(len(myHash.table)+1):
  #  print("Key: {} and Package: {}".format(i + 1, myHash.search(i + 1)))
